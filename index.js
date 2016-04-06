f10dcb92242de08b93c811afed45e803ed37686d
whiteh <greenwma@googlemail.com>
2016-04-06 19:11:51 +0000

    basic search working

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var Botkit = require('botkit');
 var fs = require("fs");
 var params = {};
+var http = require("https");
 
 var API_KEY = fs.readFileSync("api_key", "utf8");
 
 var controller = Botkit.slackbot({
-  debug: true
+  debug: false
   //include "log: false" to disable logging
   //or a "logLevel" integer from 0 to 7 to adjust logging verbosity
 });
@@ -15,7 +16,50 @@ controller.spawn({
   token: API_KEY,
 }).startRTM()
 
+callback = function(response) {
+  var str = '';
+
+  //another chunk of data has been recieved, so append it to `str`
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  //the whole response has been recieved, so we just print it out here
+  response.on('end', function (test) {
+    console.log("test: "+test);
+    bot.reply(message,'Hello yourself.')
+
+  });
+}
+
+function frinkiac(term, callback) {
+
+  http.get({
+      host: 'frinkiac.com',
+      path: '/api/search?q=rappin%27%20ronnie%20regan'},
+           function(response){
+              var body = "";
+              response.on("data", function(d) {
+                body += d;
+              });
+
+              response.on("end", function(){
+                var json = JSON.parse(body);
+                console.log(body);
+                callback(json);
+              })
+              });
+}
+
 // give the bot something to listen for.
 controller.hears('hello',['direct_message','direct_mention','mention'],function(bot,message) {
-  bot.reply(message,'Hello yourself.');
+  /*http.request({
+      host: 'frinkiac.com',
+      path: '/api/search?q=rappin%27%20ronnie%20regan'}, callback).end("blah");*/
+  //bot.reply(message,'Hello yourself.');
+  frinkiac("", function(json){
+    bot.reply(message, "Hello Yourself "+
+              "https://frinkiac.com/img/"+json[0].Episode+"/"+json[0].Timestamp+".jpg");
+  })
+
 });
